84a9b0cade9119cb05d7d6b4a672a692c4c75dce
azamkan6699-eng <azamkan6699@gmail.com>
2025-12-02 23:17:48 -0800

    Update dependencies and main.tsx setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { HelmetProvider } from "react-helmet-async"; // ✅ correct import
+import { HelmetProvider } from "react-helmet-async"; 
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
